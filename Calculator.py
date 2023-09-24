ddd60d6dc67213317d2be192f1c9b39b2792924f
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-09-24 14:04:51 +0530

    update.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,31 @@
-user_input_1 = int()
+def Calculator():
+    show_operator = ("Addition = +" , 
+                    "Subtract = -" ,
+                    "Multiply = *", 
+                    "Divide =   /" , 
+                    "Power =   **" )
+    print(show_operator)
+    print()
+    operator = input("Enter the operators you want : ")
+        
+    user_input_1 = int(input("Enter the First Number   : "))
+
+    user_input_2 = int(input("Enter the second Number  : "))
+
+    try:
+        if operator == "+" : 
+            print(user_input_1 + user_input_2)
+        elif operator == "-":
+            print(user_input_1 - user_input_2)
+        elif operator == "*":
+            print(user_input_1 * user_input_2)
+        elif operator == "/":
+            print(user_input_1 / user_input_2)
+        elif operator == "**":
+            print(user_input_1 ** user_input_2)
+        else:
+            print("Invalid Entry ")
+    except:
+        print(TypeError)
+
+Calculator()
